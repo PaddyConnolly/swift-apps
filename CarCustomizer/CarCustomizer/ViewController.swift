23fc521bcf78c562841b2ac4eb698a1c1cce091b
PatrickConnolly1 <connolly.p@etoncollege.org.uk>
2020-01-13 19:06:16 +0000

    Customization options

--- CarCustomizer/CarCustomizer/ViewController.swift
+++ CarCustomizer/CarCustomizer/ViewController.swift
@@ -11,13 +11,24 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    @IBOutlet weak var carstatistics: UILabel!
 
+    @IBOutlet var drivetrainPackage: UISwitch!
+    @IBOutlet var fuelSystemPackage: UISwitch!
+    @IBOutlet var remainingFundsDisplay: UILabel!
+    @IBOutlet var carstatistics: UILabel!
+    @IBOutlet var engineAndExhaustPackage: UISwitch!
+    @IBOutlet var tiresPackage: UISwitch!
+    var remainingFunds = 1000 {
+        didSet {
+            remainingFundsDisplay.text = "Remaining Funds: \(remainingFunds)"
+        }
+    }
     var starterCars = StarterCars()
     var carIndex = 0
     var car: Car? {
         didSet {
             carstatistics.text = car?.displayStats()
+            disableUnafforablePackages()
         }
     }
     
@@ -27,19 +38,94 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         car = starterCars.cars[carIndex]
+        remainingFunds = 1000
+        
         
     }
     
+    
     @IBAction func cycleCars(_ sender: Any) {
         carIndex += 1
+        remainingFunds = 1000
         if carIndex >= starterCars.cars.count {
             carIndex = 0
         }
+        engineAndExhaustPackage.isOn = false
+        tiresPackage.isOn = false
+        fuelSystemPackage.isOn = false
+        drivetrainPackage.isOn = false
+ 
+        engineAndExhaustPackage.isEnabled = true
+        tiresPackage.isEnabled = true
+        fuelSystemPackage.isEnabled = true
+        drivetrainPackage.isEnabled = true
+        
         car = starterCars.cars[carIndex]
 
     }
+
     
+    @IBAction func engineAndExhaustToggle(_ sender: Any) {
 
+        if engineAndExhaustPackage.isOn {
+            car?.topSpeed += 5
+            remainingFunds -= 500
+        } else {
+            car?.topSpeed -= 5
+            remainingFunds += 500
+        }
+    }
+    
+    @IBAction func tiresPackageToggle(_ sender: Any) {
 
+        if tiresPackage.isOn {
+            car?.handling += 1
+            remainingFunds -= 500
+        } else {
+            car?.handling -= 1
+            remainingFunds += 500
+        }
+    }
+    
+    @IBAction func fuelSystemPackageToggle(_ sender: Any) {
+
+        if fuelSystemPackage.isOn {
+            car?.topSpeed += 2
+            car?.acceleration -= 0.5
+            remainingFunds -= 500
+        } else {
+            car?.topSpeed -= 2
+            car?.acceleration += 0.5
+            remainingFunds += 500
+        }
+    }
+    @IBAction func drivetrainPackageToggle(_ sender: Any) {
+
+        if drivetrainPackage.isOn {
+            car?.acceleration -= 1
+            remainingFunds -= 500
+        } else {
+            car?.acceleration += 1
+            remainingFunds += 500
+        }
+    }
+    
+    func disableUnafforablePackages() {
+        engineAndExhaustPackage.isEnabled = enable(engineAndExhaustPackage)
+        tiresPackage.isEnabled = enable(tiresPackage)
+        fuelSystemPackage.isEnabled = enable(fuelSystemPackage)
+        drivetrainPackage.isEnabled = enable(drivetrainPackage)
+    }
+    func enable(_ control: UISwitch) -> Bool {
+        if control.isOn {
+            return true
+        } else {
+            if remainingFunds >= 500 {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
 }
 
